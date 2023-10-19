806cf66d7e432f0e8a9af09c4a52244e1ba4a25b
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-10-19 11:12:59 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/eriderentcarinventory/ERideRentCarInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderentcarinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideRentCarInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERideRentCarInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piotrgrochowiecki/eriderentcarinventory/ERideRentCarInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderentcarinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERideRentCarInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
